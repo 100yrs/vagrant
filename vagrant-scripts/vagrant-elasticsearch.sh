95b99fc1de35ea8de5ff8815a835ee02670d25b1
Kara Todd <kara@voxel-fox.com>
2016-03-22 20:20:32 -0400

    updates to elastic search installation

--- vagrant-scripts/vagrant-elasticsearch.sh
+++ vagrant-scripts/vagrant-elasticsearch.sh
@@ -1,26 +1,45 @@
 #!/usr/bin/env bash
 
 # Install ElasticSearch
-echo ">>> Installing Elasticsearch 2.0"
+echo ">>> Installing Elasticsearch 2.2"
 
-# install openjdk-7 
+
+# install Java, remove openjdk if present.
 sudo apt-get purge openjdk*
-sudo apt-get -y install openjdk-7-jre
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update -qq
+sudo apt-get -y install oracle-java8-installer
+
+if which java >/dev/null; then
+    echo ">>> Java already installed. Skip."
+else
+    echo ">>> Install Java 8"
+    add-apt-repository ppa:webupd8team/java
+    apt-get update -qq
+    echo debconf shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
+    echo debconf shared/accepted-oracle-license-v1-1 seen true | /usr/bin/debconf-set-selections
+    apt-get install --yes oracle-java8-installer
+    yes "" | apt-get -f install
+fi
+
 
 # Install elasticsearch
+echo ">>> Install Elastcisearch 2.2"
 wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 curl -L -O https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-2.2.0.deb
-sudo apt-get update
 sudo dpkg -i elasticsearch-2.2.0.deb
 
 # Add as service
+echo ">>> Configure Elasticsearch as service"
 sudo update-rc.d elasticsearch defaults 95 10
 sudo service elasticsearch start
 
 # Access "localhost:9200" from the host os
-sudo echo "network.host: 0.0.0.0" >> /etc/elasticsearch/elasticsearch.yml
+sudo grep -q -F 'network.host: 0.0.0.0' /etc/elasticsearch/elasticsearch.yml || echo "network.host: 0.0.0.0" >> /etc/elasticsearch/elasticsearch.yml
 
 # Enable cors (to be able to use Sense)
-sudo echo "http.cors.enabled: true" >> /etc/elasticsearch/elasticsearch.yml
-sudo echo "http.cors.allow-origin: /https?:\/\/.*/" >> /etc/elasticsearch/elasticsearch.yml
+sudo grep -q -F 'http.cors.enabled: true' /etc/elasticsearch/elasticsearch.yml || echo "http.cors.enabled: true" >> /etc/elasticsearch/elasticsearch.yml
+sudo grep -q -F 'http.cors.allow-origin: /https?:\/\/.*/' /etc/elasticsearch/elasticsearch.yml || echo "http.cors.allow-origin: /https?:\/\/.*/" >> /etc/elasticsearch/elasticsearch.yml
+
+echo ">>> Restart Elasticsearch"
 sudo service elasticsearch restart
